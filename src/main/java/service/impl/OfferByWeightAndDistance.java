03f20eb7d6f9c33d871dc19353336fa8f759cda0
Sainath Bottupally <sainath.bottupally@thoughtworks.com>
2022-05-21 11:18:57 +0530

    Implement offer criteria by distance and weight

--- src/main/java/service/impl/OfferByWeightAndDistance.java
+++ src/main/java/service/impl/OfferByWeightAndDistance.java
@@ -1,11 +1,20 @@
 package service.impl;
 
+import lombok.RequiredArgsConstructor;
 import model.CourierPackage;
 import model.OfferCriteria;
 
+@RequiredArgsConstructor
 public class OfferByWeightAndDistance implements OfferCriteria {
+
+    private final int minimumDistance;
+    private final int maximumDistance;
+    private final int minimumWeight;
+    private final int maximumWeight;
+
     @Override
     public boolean isCouponApplicableFor(CourierPackage courierPackage) {
-        return false;
+        return courierPackage.getPackageWeight() >= minimumWeight && courierPackage.getPackageWeight() <= maximumWeight &&
+                courierPackage.getDeliveryDistance() >= minimumDistance && courierPackage.getDeliveryDistance() <= maximumDistance;
     }
 }
--- src/test/java/service/impl/OfferByWeightAndDistanceTest.java
+++ src/test/java/service/impl/OfferByWeightAndDistanceTest.java
@@ -5,10 +5,11 @@ import model.OfferCriteria;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class OfferByWeightAndDistanceTest {
 
-    private final OfferCriteria offerCriteria = new OfferByWeightAndDistance();
+    private final OfferCriteria offerCriteria = new OfferByWeightAndDistance(0, 200, 70, 200);
 
     @Test
     void shouldReturnFalseForCoupon() {
@@ -17,5 +18,14 @@ class OfferByWeightAndDistanceTest {
         boolean couponApplicable = offerCriteria.isCouponApplicableFor(courierPackage);
 
         assertFalse(couponApplicable);
+    }
+
+    @Test
+    void shouldReturnTrueForCoupon() {
+        CourierPackage courierPackage = CourierPackage.builder().packageId("PKG1").packageWeight(80).deliveryDistance(100).couponCode("OFR003").build();
+
+        boolean couponApplicable = offerCriteria.isCouponApplicableFor(courierPackage);
+
+        assertTrue(couponApplicable);
     }
 }
